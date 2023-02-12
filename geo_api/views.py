b69367f7d58d63e65b389c9af4a376f751b97684
Freeman Smith <freemanwsmith3@gmail.com>
2023-02-12 17:17:12 -0500

    other guess method with browser storage only

--- geo_api/views.py
+++ geo_api/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from geo.models import Country, Riddle
 from .serializers import CountrySerializer, RiddleSerializer
 from rest_framework.permissions import IsAdminUser
 from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 class CountryList(generics.ListCreateAPIView):
     #permission_classes = [IsAdminUser]
@@ -21,7 +22,9 @@ class AnswerList(generics.ListAPIView):
 
 class RiddleRetrieve(generics.RetrieveAPIView):
     #permission_classes = [IsAdminUser]
-    queryset = Riddle.objects.all()
-    serializer_class = RiddleSerializer
 
+    def get(self, request):
+        queryset = Riddle.objects.all()
+        serializer_class = RiddleSerializer
+        return Response(status=status.HTTP_201_CREATED)
 
