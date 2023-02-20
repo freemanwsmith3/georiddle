689a68ac4dbed5650c1fc75b7317f0c4dcd124e2
Freeman Smith <freemanwsmith3@gmail.com>
2023-02-20 17:24:09 +0100

    moved directory back to index

--- core/settings.py
+++ core/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'static'/'react'/'geoapi'/'src'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
