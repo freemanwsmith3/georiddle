4b1166948a7ffd489139946a841dea189439759d
Freeman Smith <freemanwsmith3@gmail.com>
2023-03-07 18:32:58 -0600

    env file added with config

--- core/settings.py
+++ core/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
-import pyodbc
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,7 +90,7 @@ DATABASES = {
    'ENGINE': 'django.db.backends.postgresql_psycopg2',
    'NAME': 'postgres',
    'USER': 'freemanwsmith3',
-   'PASSWORD': os.environ['PASSWORD'],
+   'PASSWORD': config('PASSWORD'),
    'HOST': 'georiddlepgserver.postgres.database.azure.com',
    'PORT': '5432'
   }
