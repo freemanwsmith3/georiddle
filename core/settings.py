e2e1d5658c79ae036dd7fa512b657526c343ee11
Freeman Smith <freemanwsmith3@gmail.com>
2023-03-21 15:38:44 -0500

    put csrf same site true also:

--- core/settings.py
+++ core/settings.py
@@ -190,14 +190,8 @@ SESSION_ENGINE  = "django.contrib.sessions.backends.signed_cookies"
 # CSRF_COOKIE_SECURE = True
 
 ### maybe need this in addition to thing below
+
+CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-
-SESSION_COOKIE_SAMESITE= 'None'
-
-
 CSRF_COOKIE_SAMESITE = 'None'
-# SESSION_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_HTTPONLY = False
-CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOW_CREDENTIALS = True
+SESSION_COOKIE_SAMESITE = 'None'
