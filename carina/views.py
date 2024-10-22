3e9d30c7510c3c595ab8c3e576f791f8f56ea131
Freeman Smith <freemanwsmith3@gmail.com>
2024-10-21 19:05:02 -0500

    carina gift

--- carina/views.py
+++ carina/views.py
@@ -5,6 +5,7 @@ from .models import Content
 def get_content(request, code):
     content = get_object_or_404(Content, code=code)
     data = {
+        'id': content.id,
         'image_url': content.image_url,
         'title': content.title,
         'description': content.description
